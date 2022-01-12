161ac06b9a4b0d5bc470eca24746dce65a2b6857
Albert Acosta <albert.jb.kf@gmail.com>
2022-01-12 14:16:33 -0300

    agregamos print tipo de variable

--- 01.introduccion/02.variables.php
+++ 01.introduccion/02.variables.php
@@ -2,26 +2,32 @@
 
 #Variable numerica
 $numero_cinco = 5;
-echo "Esto es una variable numero: $numero_cinco";
+echo "Esto es una variable numero: $numero_cinco<br>";
+var_dump($numero_cinco);
 echo "<br><br>";
 #Variable de texto
 $palabra = "Hola mundo";
-echo "Esto es una variable de texto: $palabra";
+echo "Esto es una variable de texto: $palabra<br>";
+var_dump($palabra);
 echo "<br><br>";
 #Variable boleana
 $boleano = true;
-echo "Esto es una variable boleana: $boleano";
+echo "Esto es una variable boleana: $boleano<br>";
+var_dump($boleano);
 echo "<br><br>";
 #Variable de arreglo
 $colores = array("azul","verde","amarillo");
-echo "Esto es una variable arreglo: $colores[0]";
+echo "Esto es una variable arreglo: $colores[0]<br>";
+var_dump($colores);
 echo "<br><br>";
 #Variable de arreglo con propiedades
 $verduras = array("verdura1"=>"tomate", "verdura2"=>"papas", "verdura3"=>"locote");
-echo "Esto es una variable arreglo con propiedades: $verduras[verdura1]";
+echo "Esto es una variable arreglo con propiedades: $verduras[verdura1]<br>";
+var_dump($verduras);
 echo "<br><br>";
 #Variables Objetos
 $frutas = (object) ["fruta1"=>"pera", "fruta2"=>"manzana", "fruta3"=>"banana"];
-echo "Esto es una variable objeto: $frutas->fruta2";
+echo "Esto es una variable objeto: $frutas->fruta2<br>";
+var_dump($frutas);
 
 ?>
